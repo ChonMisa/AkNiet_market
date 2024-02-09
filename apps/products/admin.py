b47a8ef885458b7ddecb7ab8e137046404af5b88
Your Name <you@example.com>
2024-02-09 19:27:36 +0600

    first

--- apps/products/admin.py
+++ apps/products/admin.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
-from apps.products.models import Product
-from apps.products.models import ProductImage
-#
-# class ProductImageInline(admin.TabularInline):
-#     model = Product.get_images().through
-#     extra = 1
+from apps.products.models import Product, ProductImage
+
+
+class ProductImageInline(admin.TabularInline):
+    model = ProductImage
+    extra = 1
 
 
 @admin.register(ProductImage)
@@ -17,3 +17,4 @@ class ProductAdmin(admin.ModelAdmin):
     list_display = ['title', 'price']
     list_filter = ['category']
     search_fields = ['title']
+    inlines = [ProductImageInline]
